3a4e362c7b667b5c9c4204aaafc1a882f8352024
thaviruh <thaviru.20221881@iit.ac.lk>
2024-05-01 17:25:55 +0530

    chore: add log in buttons

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -1,13 +1,16 @@
 "use client";
 
 import { useConvexAuth } from "convex/react";
+import { SignInButton, UserButton } from "@clerk/clerk-react";
+import Link from "next/link";
 
 import { useScrollTop } from "@/hooks/use-scroll-top";
-import { cn } from "@/lib/utils";
-import { Logo } from "./logo";
 import { ModeToggle } from "@/components/mode-toggle";
-import { SignInButton } from "@clerk/clerk-react";
 import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/spinner";
+import { cn } from "@/lib/utils";
+
+import { Logo } from "./logo";
 
 export const Navbar = () => {
   const { isAuthenticated, isLoading } = useConvexAuth();
@@ -23,9 +26,7 @@ export const Navbar = () => {
       <Logo />
       <div className="md:ml-auto md:justify-end justify-between w-full flex items-centers gap-x-2">
         {isLoading && (
-            <p>
-                Loading...
-            </p>
+            <Spinner />
         )}
         {!isAuthenticated && !isLoading && (
             <>
@@ -40,6 +41,19 @@ export const Navbar = () => {
                     </Button>
                 </SignInButton>
             </>
+        )}
+        {isAuthenticated && !isLoading && (
+           <>
+            <Button variant="ghost" size="sm" asChild>
+                <Link href="/documents">
+                    Enter Scribblix
+                </Link>
+            </Button>
+            <UserButton 
+                afterSignOutUrl="/"
+            />
+           </> 
+            
         )}
         <ModeToggle />
       </div>
